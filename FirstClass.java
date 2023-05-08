c199f57995710878c92de00139b7dc94c20f11c1
Shivam Vikash <131040493+ShivamVikash@users.noreply.github.com>
2023-05-08 22:11:07 +0530

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,21 @@
+class FirstClass 
+{ 
+   public static void main(String agrs[])
+   {
+      //learning to print output
+
+      System.out.print("Hello \nWorld ");
+      System.out.println("Hello World\n");
+
+      //learning variables
+
+      String name = "Alone Entity";
+      int age = 19;
+      double price = 3677129;
+      age = 25;
+      price = 77529963;
+      System.out.println(name);
+      System.out.println(age);
+      System.out.println(price);
+   }
+}
