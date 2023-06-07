a2f4705533b42007c53c47521e3225612c38d4a1
Shivam Vikash <131040493+ShivamVikash@users.noreply.github.com>
2023-06-07 23:43:03 +0530

    Functions.java

--- Functions.java
+++ Functions.java
@@ -188,4 +188,146 @@ public class Functions{
         System.out.println(g+" is greater");
         
     }
+    
+    //4. Function that takes in radius as input and returns circumference as output
+    public static double returnCircumference(double r){
+        double c= 2*3.14*r;
+        return c;
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the value of radius");
+        double r= sc.nextDouble();
+        double c= returnCircumference(r);
+        System.out.println("Circumference of the circle: "+ c);
+    }
+    
+    
+    //5. Function that takes age as input and returns whether person is eligible to vote
+    public static String returnEligibility (int age){
+        String s;
+        if(age>=18){
+           // System.out.println("The person is eligible to vote");
+            s= "eligible";
+        }
+        else {
+            //System.out.println("The person is not eligible to vote");
+            s= "Not Eligible";
+        }
+        return s;
+    }
+    public static void main (String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the age");
+        int age= sc.nextInt();
+        String s= returnEligibility(age);
+        System.out.println("The person is "+s+" to vote");
+    }
+    
+    
+    //6. Infinite loop using do while condition
+    public static void main(String args[]){
+        int i=1;
+        do{
+            System.out.println("Infinite loop");
+            i++;
+        }while(i>0);
+    }
+    
+    //7. Program to input no. from the user and display the count
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the no. of times you want to input the number");
+        int n= sc.nextInt();
+        int c1=0; int c2=0; int c3=0;
+        for(int i=1; i<=n; i++){
+            System.out.println("Enter your number");
+            float num= sc.nextFloat();
+            if(num>0.0){
+                c1++;
+            }
+            else if(num<0.0){
+                c2++;
+            }
+            else{
+                c3++;
+            }
+        }
+        System.out.println("No. of positive numbers entered: "+c1);
+        System.out.println("No. of negative numbers entered: "+c2);
+        System.out.println("No. of zeros entered: "+c3);
+    }
+    
+    //8. Enter two numbers from the user and write a function to find value of one raised to power the other
+    public static void findPower(float x, float n){
+        float v= 1;
+        for(int i=1; i<=n; i++){
+            v=v*x;
+        }
+        System.out.println("Value of exponenet is: "+v);
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the value of base");
+        float x= sc.nextFloat();
+        System.out.println("Enter the value of power");
+        float n= sc.nextFloat();
+        findPower(x,n);
+    }
+    
+    //9. Function to calculate greatest common divisor
+    public static void calculateGcd(int n1, int n2){
+        if(n1>n2){
+            if(n1%n2==0){
+                System.out.println(n2+" is the greatest common divisor");
+            }
+            else{
+                for(int i=n2; i>=1; i--){
+                    if(n1%i==0 && n2%i==0){
+                        System.out.println(i+" is the greatest common divisor");
+                        break;
+                    }
+                }
+            }
+        }
+        else{
+            if(n2%n1==0){
+                System.out.println(n1+" is the greatest common divisor");
+            }
+            else{
+                for(int i=n1; i>=1; i--){
+                    if(n1%i==0 && n2%i==0){
+                        System.out.println(i+" is the greatest common divisor");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first number");
+        int n1= sc.nextInt();
+        System.out.println("Enter second number");
+        int n2= sc.nextInt();
+        calculateGcd(n1,n2);
+    }
+ 
+    //10. To print Fibonacci series of n terms
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the limit value of series");
+        int n= sc.nextInt();
+        int v1=0; int v2=1;
+        System.out.print(v1+" ");
+        System.out.print(v2+" ");
+        for(int i=1; i<=n-2; i++){
+            int s= v1+v2;
+            System.out.print(s+" ");
+            v1=v2;
+            v2=s;
+        }
+    } 
+    
 }
+
